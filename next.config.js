ba0587c3f78807139896ce0ae14ca4e3d597afbb
Snorre Fossland <snorres@gmail.com>
2020-05-20 13:59:15 +0200

    test heroku

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
-// next.config.js
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
+// // next.config.js
+// const withSass = require('@zeit/next-sass')
+// const withCSS = require('@zeit/next-css')
 
-// module.exports = withCSS(withSass(
+// // module.exports = withCSS(withSass(
+// //   {
+// //     target: "serverless",
+// //   }
+// // ))
+
+// module.exports = 
+//   withCSS(withSass(
+//     {
+//       target: "serverless",
+//     }
+//   )),
 //   {
-//     target: "serverless",
+//   webpack: (config, { isServer }) => {
+//     // Fixes npm packages that depend on `fs` module
+//     if (!isServer) {
+//       config.node = {
+//         fs: 'empty'
+//       }
+//     }
+//     return config
 //   }
-// ))
-
-module.exports = 
-  withCSS(withSass(
-    {
-      target: "serverless",
-    }
-  )),
-  {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-    return config
-  }
-}
+// }

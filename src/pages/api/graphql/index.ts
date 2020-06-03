acef45db6ed8b7eaeffdbae3875dfae60509b8d4
Snorre Fossland <snorres@gmail.com>
2020-06-03 13:22:04 +0200

    removed gql appollo-server-micro

--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -1,4 +1,5 @@
-import { ApolloServer, gql } from "apollo-server-micro";
+import { ApolloServer } from "apollo-server-micro";
+// import { ApolloServer, gql } from "apollo-server-micro";
 import Cors from "micro-cors";
 
 // import resolvers from '../../../../public/gql/resolvers'

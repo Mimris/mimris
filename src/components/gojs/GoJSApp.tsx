ce707ad67e6fa7f2867e3bea28dd1456be9be8cb
Snorre Fossland <snorres@gmail.com>
2020-07-07 08:59:56 +0200

    fix

--- src/components/gojs/GoJSApp.tsx
+++ src/components/gojs/GoJSApp.tsx
@@ -209,7 +209,7 @@ class GoJSApp extends React.Component<{}, AppState> {
                     const gqlNode = new gql.gqlObjectView(myNode.objectview);
                     modifiedNodes.push(gqlNode);
                     const gqlObj = new gql.gqlObject(myNode.objectview.object);
-                    //modifiedObjects.push(gqlObj);
+                    modifiedObjects.push(gqlObj);
                   }
                 }
               }

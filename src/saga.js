afc5b7d4985a1e41f178d83b49319b90e19b4323
Snorre Fossland <snorres@gmail.com>
2020-12-01 18:45:02 +0100

    changed to mode: no-cors in saga

--- src/saga.js
+++ src/saga.js
@@ -88,7 +88,7 @@ function * loadDataModelListSaga() {
     let res = ''  
     res = yield fetch(`${akmmhost}akm-model-list/`,
       {
-        // mode: 'no-cors', // comment in for  server login
+        mode: 'no-cors', // comment in for  server login
         headers: {
           // "Access-Control-Allow-Origin": "*", // comment in for  server login
           'Accept': 'application/json',

56a66514219bb10660b2db20d7029156a84c381e
Snorre Fossland <snorres@gmail.com>
2021-02-16 12:25:15 +0100

    added video

--- src/components/SelectVideo.tsx
+++ src/components/SelectVideo.tsx
@@ -5,23 +5,47 @@ import { useSelector, useDispatch } from 'react-redux'
 import Selector from './utils/Selector'
 import { loadState, saveState } from './utils/LocalStorage'
 import { FaJoint } from 'react-icons/fa';
+import Select from "react-select"
 import { StartVideo } from '../defs/StartVideo'
 const SelectContext = (props: any) => {
   // console.log('8 8', props.modal);
-  const dispatch = useDispatch()
-  let state = useSelector((state:any) => state) // Selecting the whole redux store
-  const metamodels = useSelector(metamodels => state.phData?.metis?.metamodels)  // selecting the models array
-  
-  const { buttonLabel, className } = props;
-  const [modal, setModal] = useState(false);
-  const toggle = () => setModal(!modal);
-  
+  // let state = useSelector((state:any) => state) // Selecting the whole redux store
+  // const dispatch = useDispatch()
+
+  const [video, setVideo] = useState('/videos/AKMM-Getting-Started-1.mp4')
+
+  // const videos = [{videoURI: '/videos/AKMM-Getting-Started-1.mp4'},{videoURI: '/videos/snorres.mp4'}]
+  const videos = [
+    {label: "/videos/AKMM-Getting-Started-1.mp4", value: '/videos/AKMM-Getting-Started-1.mp4'},
+    {label: "/videos/AKMM-NewModel-EditProject1.mp4", value: '/videos/AKMM-NewModel-EditProject1.mp4'}]
+  // const videoURIs = videos.map(v => v.videoURI)
+
+  let startVideoDiv = <StartVideo videoURI={video} />
+  useEffect(() => { 
+    console.log('23', video);
+    
+     startVideoDiv =    <StartVideo videoURI={video} />
+  }, [video])
+
+  const handleSelectVideo = (value) => {
+    console.log('29', value);
+    setVideo(value.value)
+    console.log('31', video);
+  }
+
   return (
     <>
       {/* <button className="btn-context btn-link float-right mb-0 pr-2" size="sm" color="link" onClick={toggle}>{buttonLabel}
       </button>    */}
         <h4> Instruction Videos : Get Started </h4>
-          <StartVideo videoURI={'/videos/snorres.mp4'} />
+
+          <Select className="video-select w-25 mb-2 float-left"
+            options={videos}
+            value={video}
+            onChange={value => handleSelectVideo(value)}
+          />
+          <span className="float-right text-white">{video}</span>
+          {startVideoDiv}
       <style jsx>{`
       `}</style> 
     </>
--- src/defs/StartVideo.tsx
+++ src/defs/StartVideo.tsx
@@ -4,38 +4,11 @@ import { useState } from 'react';
 export const StartVideo = (props) => {
 
   console.log('6', props);
-  
-
-  const [AutoPlay, setAutoPlay] = useState({});
-  const Videos = [props.videoURI]
-
-  const onClickHandler = id => {
-    if (AutoPlay.hasOwnProperty(id)) {
-      if (AutoPlay[id] === true) {
-       setAutoPlay({...AutoPlay,[id]:false});    
-      } else {
-        setAutoPlay({...AutoPlay,[id]:true});
-      }
-    } else {
-      setAutoPlay({...AutoPlay,[id]:true});
-    }
-  }
-  console.log('23', Videos);
-  
   return (
     <>
-      {
-        Videos &&  Videos.map((result,index) => {
-          return (
-            <>
-              <video controls key={index} autoplay={AutoPlay[index]===true}>
-                <source src={props.videoURI} type="video/mp4" />
-              </video>
-              {/* <button onClick={() => onClickHandler(index)}>Play</button> */}
-            </>
-          )
-        })
-      }
+      <video key={props.videoURI} controls>
+        <source src={props.videoURI} type="video/mp4" />
+      </video>
     </>
   )
 }
@@ -43,3 +16,36 @@ export const StartVideo = (props) => {
 
 
 
+
+// const [AutoPlay, setAutoPlay] = useState({});
+// const Videos = [props.videoURI.videos]
+
+// const onClickHandler = id => {
+//   if (AutoPlay.hasOwnProperty(id)) {
+//     if (AutoPlay[id] === true) {
+//      setAutoPlay({...AutoPlay,[id]:false});    
+//     } else {
+//       setAutoPlay({...AutoPlay,[id]:true});
+//     }
+//   } else {
+//     setAutoPlay({...AutoPlay,[id]:true});
+//   }
+// }
+// console.log('23', Videos);
+
+// return (
+//   <>
+//     {
+//       Videos &&  Videos.map((result,index) => {
+//         return (
+//           <> 
+//             <video controls key={index} autoplay={AutoPlay[index]===true}>
+//               <source src={result} type="video/mp4" />
+//             </video>
+//             {/* <button onClick={() => onClickHandler(index)}>Play</button> */}
+//           </>
+//         )
+//       })
+//     }
+//   </>
+// )

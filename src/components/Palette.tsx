f3d047a0beabb8b40c9fc92754f6801533392d9b
Snorre Fossland <snorres@gmail.com>
2022-10-14 09:55:20 -0300

    fix

--- src/components/Palette.tsx
+++ src/components/Palette.tsx
@@ -61,7 +61,7 @@ const Palette = (props: any) => {
   
   //rearrange sequence
   let ndarr = props.gojsMetamodel?.nodeDataArray
-  if (debug) console.log('65 propsMetamodel', model?.name, mmodel?.name, ndarr);
+  if (!debug) console.log('65 propsMetamodel', model?.name, mmodel?.name, ndarr);
   let taskNodeDataArray: any[] = ndarr
 
   if (focusTask) {
@@ -77,8 +77,19 @@ const Palette = (props: any) => {
   useEffect(() => {
     isRendered = true;
     if (isRendered) {
-      if (debug) clog('80Palette useEffect', props);
-    (mmodel) && genRoleTasks(mmodel, dispatch)
+      if (!debug) clog('80Palette useEffect', props);
+      (mmodel) && genRoleTasks(mmodel, dispatch)
+    }
+    if (props.phFocus.focusTask.workOnTypes) {
+      taskNodeDataArray = props.phFocus.focusTask?.workOnTypes?.map((wot: any) => // list of types for this focusTask (string)
+        ndarr?.find((i: { typename: any; }) => {
+          return (i?.typename === wot) && i 
+        })
+      ).filter(Boolean) // remove undefined
+      if (debug) console.log('93 taskNodeDataArray', taskNodeDataArray);
+    
+    if (!debug) console.log('91 taskNodeDataArray', taskNodeDataArray, taskNodeDataArray)
+    if (!debug) console.log('94 seltasks', props.phFocus.focusTask)
     }
     return () => { isRendered = false; }
   }, [])

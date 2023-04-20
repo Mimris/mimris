9b88152a79b75cb8c548a3f3c592f1237f55b8a5
dagkarlsen <dag.karlsen@folkero.net>
2023-04-20 15:19:34 +0200

    Fix

--- src/akmm/ui_modal.ts
+++ src/akmm/ui_modal.ts
@@ -766,7 +766,8 @@ export function handleCloseModal(selectedData: any, props: any, modalContext: an
       if (debug) console.log('625 obj, jsnObject, node', obj, jsnObject, node);
       const n = myDiagram.findNodeForKey(node.key)
       const data = n ? n.data : node.data;
-      if (!debug) console.log('628 node', node);
+      if (debug) console.log('628 node', node);
+      // Special handling of the Draft property
       if (node.Draft) {
         myDiagram.model.setDataProperty(data, 'typename', node.Draft);
       }

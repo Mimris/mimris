c86ab2a23c3d98fb16273d81a128496c4c5ac49e
dagkarlsen <dag.karlsen@folkero.net>
2023-07-16 10:43:35 +0200

    Fixes

--- src/akmm/ui_common.ts
+++ src/akmm/ui_common.ts
@@ -4,7 +4,6 @@ const debug = false;
 import * as utils from './utilities';
 import * as akm from './metamodeller';
 import * as uid from './ui_diagram';
-import * as uic from './ui_common';
 import * as gjs from './ui_gojs';
 import * as jsn from './ui_json';
 const constants = require('./constants');
@@ -1820,6 +1819,7 @@ export function onLinkRelinked(lnk: gjs.goRelshipLink, fromNode: any, toNode: an
         if (fromNode && toNode) {
             const myDiagram = context.myDiagram;
             const myMetis = context.myMetis;
+            const myModelview = context.myModelview;
             const myGoModel = context.myGoModel;
             const link = myGoModel.findLink(lnk.key) as gjs.goRelshipLink;
             if (debug) console.log('1801 lnk, link', lnk, link);
@@ -1841,11 +1841,14 @@ export function onLinkRelinked(lnk: gjs.goRelshipLink, fromNode: any, toNode: an
                     // After relink   
                     link.category = lnk.category;
                     link.name = name;        
-                    link.setFromNode(lnk.from);
                     rel.fromPortid = lnk.fromPort;
                     rel.toPortid = lnk.toPort;
                     relview.fromPortid = lnk.fromPort;
                     relview.toPortid = lnk.toPort;
+                    link.from = lnk.from;
+                    link.to = lnk.to;
+                    link.fromNode = fromNode
+                    link.toNode = toNode;
                     link.fromPort = lnk.fromPort;
                     link.toPort = lnk.toPort;
                     const fromObjView = fromNode.objectview;
@@ -1853,7 +1856,6 @@ export function onLinkRelinked(lnk: gjs.goRelshipLink, fromNode: any, toNode: an
                     relview.fromPortid = lnk.fromPort;
                     relview.toPortid = lnk.toPort;
                     rel.fromObject = myMetis.findObject(fromObjView.object.id);  
-                    link.setToNode(lnk.to);
                     const toObjView = toNode.objectview;
                     relview.toObjview = toObjView;
                     rel.toObject = myMetis.findObject(toObjView.object.id); 
@@ -1864,8 +1866,12 @@ export function onLinkRelinked(lnk: gjs.goRelshipLink, fromNode: any, toNode: an
                     if (debug) console.log('1840 fromobj2, toObj2', fromObj2, toObj2);  
                     const myLink = myDiagram.findLinkForKey(link.key);
                     myLink.category = link.template;
-                    uic.setLinkProperties(relview, myMetis, myDiagram);
+                    setLinkProperties(relview, myMetis, myDiagram);
                     // Do the dispatches          
+                    const jsnFromObj = new jsn.jsnObject(fromObj2);
+                    context.modifiedObjects.push(jsnFromObj);
+                    const jsnToObj = new jsn.jsnObject(toObj2);
+                    context.modifiedObjects.push(jsnToObj);
                     const jsnRelview = new jsn.jsnRelshipView(relview);
                     context.modifiedRelshipViews.push(jsnRelview);
                     const jsnRel = new jsn.jsnRelationship(rel);
@@ -2178,19 +2184,18 @@ export function setLinkProperties(relview: akm.cxRelationshipView, myMetis: akm.
     const fromObjview = relview.fromObjview;
     const toObjview = relview.toObjview;
     if (fromObjview && toObjview) {
-        let link = uid.getLinkByViewId(relview.id, myDiagram);
-        if (!link) {
+        let link = myGoModel.findLinkByViewId(relview.id);
+        if (!link)
             link = new gjs.goRelshipLink(utils.createGuid(), myGoModel, relview);
-            link.loadLinkContent(myGoModel);
-            link.fromNode = uid.getNodeByViewId(fromObjview.id, myDiagram);
-            link.from = link.fromNode?.key;
-            link.toNode = uid.getNodeByViewId(toObjview.id, myDiagram);
-            link.to = link.toNode?.key;
-            myGoModel.addLink(link);
-            myDiagram.startTransaction('add link')
-            myDiagram.model.addLinkData(link);
-            myDiagram.commitTransaction('add link');
-        }
+        link.loadLinkContent(myGoModel);
+        link.fromNode = uid.getNodeByViewId(fromObjview.id, myDiagram);
+        link.from = link.fromNode?.key;
+        link.toNode = uid.getNodeByViewId(toObjview.id, myDiagram);
+        link.to = link.toNode?.key;
+        myGoModel.addLink(link);
+        myDiagram.startTransaction('add link')
+        myDiagram.model.addLinkData(link);
+        myDiagram.commitTransaction('add link');        
     }
 }
 

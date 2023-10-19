83cf8e650f0d44c07adc4b35bb6c9c858a4e896e
Snorre Fossland <snorres@gmail.com>
2023-10-19 17:52:13 +0200

    fix

--- src/akmm/ui_templates.ts
+++ src/akmm/ui_templates.ts
@@ -3269,7 +3269,7 @@ export function findImage(image: string) {
         const img = {image:'data:image/svg+xml;charset=UTF-8,image'}
         if (!debug) console.log('3269', img);
         return img
-    } else { 
+    } else if (image !== '') { 
         const img = "images/types/" + image
         if (!debug) console.log('3273 Diagram', image, img)
         return img //its an image in public/images

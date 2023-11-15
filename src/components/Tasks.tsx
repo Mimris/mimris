2749ef12109b3ffa1cd131e946f51a1bbd2ddf0d
Snorre Fossland <snorres@gmail.com>
2023-11-15 18:07:46 +0100

    fix

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -310,7 +310,7 @@ function Tasks(props) {
   const genTasksDiv = () => {
     let parent, topGroupOvsDiv;
     if (!mothermodelviews) return null;
-    console.log('371 Tasks', mothermodelviews, mothermodel.objects);
+    if (debug) console.log('371 Tasks', mothermodelviews, mothermodel.objects);
     const modview = 
       mothermodelviews?.map((mv: any, index: number) => { // map over all modelviews of this model
         if (debug) console.log('371 Tasks', mv);

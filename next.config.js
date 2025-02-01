a3b311c66e08a49279d015bd98a28ab59f0f800b
Snorre Fossland <snorres@gmail.com>
2025-02-01 11:57:40 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,26 +1,43 @@
-
-module.exports = {
-    // webpack: (config) => {
-
-    //   return config
-    // }
-    reactStrictMode: false,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.node = {
-        // fs: 'empty'
-        global: true,
-        __filename: true,
-        __dirname: true,
-      }
-    
-    return config
-  }
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   config.node = {
-  //     fs: 'empty'
-  //   }
-  //   return config
-  // }
-}
 const removeImports = require('next-remove-imports')();
-module.exports = removeImports({});
+
+const nextConfig = {
+  reactStrictMode: false,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Ensure that custom webpack configurations are preserved.
+    config.node = {
+      global: true,
+      __filename: true,
+      __dirname: true,
+    };
+    return config;
+  },
+};
+
+module.exports = removeImports(nextConfig);
+
+
+// module.exports = {
+//     // webpack: (config) => {
+
+//     //   return config
+//     // }
+//     reactStrictMode: false,
+//     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//       config.node = {
+//         // fs: 'empty'
+//         global: true,
+//         __filename: true,
+//         __dirname: true,
+//       }
+    
+//     return config
+//   }
+//   // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+//   //   config.node = {
+//   //     fs: 'empty'
+//   //   }
+//   //   return config
+//   // }
+// }
+// const removeImports = require('next-remove-imports')();
+// module.exports = removeImports({});
